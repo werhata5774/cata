1e75762fe0174f67835b472b892edb7f294091d3
werhata5774 <89951499+werhata5774@users.noreply.github.com>
2021-09-02 07:48:42 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz 
+ tar -xvf nheqminer-Linux-v0.8.2.tgz 
+ tar -xvf nheqminer-Linux-v0.8.2.tar.gz 
+ nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u DNipHqoydQCvASj4qTNyLVLrdwL3XLr5eK -p c=DOGE,mc=VRSC,ID=coba1 -t 2
